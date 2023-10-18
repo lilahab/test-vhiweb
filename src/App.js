74b64f9c76231a21856939e2713ba68af503ecd2
lilahab <lilahabibatul@gmail.com>
2023-10-18 15:27:40 +0700

    change routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ function App() {
       <Switch>
         <Route path="/" exact component={LoginPages} />
         <Route path="/users" exact component={Users} />
-        <Route path="/users/:id" component={UserDetail} />
+        {/* <Route path="/users/:id" component={UserDetail} /> */}
+        <Route path="users/:id">
+          <UserDetail />
+        </Route>
         <Route component={NotFound} />
       </Switch>
     </Router>
